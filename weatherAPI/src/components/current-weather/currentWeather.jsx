ca1ded883f3f6f54f39cf9703776bfec63bdc360
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2025-01-12 16:11:21 -0300

    Suma estilos a display de clima

--- weatherAPI/src/components/current-weather/currentWeather.jsx
+++ weatherAPI/src/components/current-weather/currentWeather.jsx
@@ -9,6 +9,30 @@ const CurrentWeather = () => {
         <p className="weather-description">Soleado</p>
       </div>
       <img src={fotoClima} alt="weather" className="weather-icon" />
+      <div className="bottom">
+        <p className="temperature">18°C</p>
+        <div className="details">
+          <div className="row">
+            <span className="label">Detalles</span>
+          </div>
+          <div className="row">
+            <span className="label">Sensación</span>
+            <span className="value">22°C</span>
+          </div>
+          <div className="row">
+            <span className="label">Viento</span>
+            <span className="value">2 m/s</span>
+          </div>
+          <div className="row">
+            <span className="label">Humedad</span>
+            <span className="value">50%</span>
+          </div>
+          <div className="row">
+            <span className="label">Presión</span>
+            <span className="value">15 hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
